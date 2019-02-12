fc55e45f3fdd12b8b3ebe1b28ff69f261ba8f40b
Tanner Black <ybb777@gmail.com>
2019-02-11 17:19:44 -0700

    Animal Assignment

---
+++ 2D_Game/Assets/Scripts/Assignments/BengalTiger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BengalTiger : Feline {
+
+
+	public bool banded;
+
+	// Use this for initialization
+	// void Start () {
+	// 	print("Roar!")
+
+	// }
+	
+}
---
+++ 2D_Game/Assets/Scripts/Assignments/GenericPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GnericPickup : MonoBehaviour {
+
+	public bool isVisable;
+	public 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
