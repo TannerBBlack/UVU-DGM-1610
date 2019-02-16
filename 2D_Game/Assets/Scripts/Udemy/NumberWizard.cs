362326689b04d3290c1dfe8c7b901846451e0144
Tanner Black <tbb777@gmail.com>
2019-02-15 17:32:49 -0700

    another push

---
+++ 2D_Game/Assets/Scripts/Udemy/NumberWizard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+        Debug.Log("Pick a number");
+        Debug.Log("Max number is 1000");
+        Debug.Log("Min number is 1");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
