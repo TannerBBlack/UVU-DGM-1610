298df44469149d6ad153d89f28e3c60b2d0fc1ac
Tanner Black <tbb777@gmail.com>
2019-02-16 20:21:51 -0700

    Made chihuahua class

---
+++ 2D_Game/Assets/Scripts/Assignments/Canine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canine : Animal {
+
+    public bool domesticated;
+    public bool floppyEars;
+    public bool fourLegs;
+    public bool shortHair;
+    public int age;
+    public float weight;
+    public float maxSpeed;
+    public string furColor;
+
+}
---
+++ 2D_Game/Assets/Scripts/Assignments/Chihuahua.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chihuahua : Canine {
+
+    public bool annoying = true;
+    public bool bigEyes;
+    public string petName;
+
+
+}
