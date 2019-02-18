ca5f080010ede61978020b25e2073b1eaa333490
Tanner Black <tbb777@gmail.com>
2019-02-17 22:35:31 -0700

    Add card scripts

---
+++ 2D_Game/Assets/Scripts/Assignments/MirrorWall.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorWall : TrapCard {
+
+    public string cardName;
+    public string effect;
+}
---
+++ 2D_Game/Assets/Scripts/Assignments/TradingCard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TradingCard : MonoBehaviour {
+
+    public bool glossy;
+    public bool rare;
+    public bool cool;
+
+}
