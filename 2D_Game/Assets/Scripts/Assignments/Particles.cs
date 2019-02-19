9463b77573097db33bd1d84ff7312a07cd4bd5d2
Tanner Black <tbb777@gmail.com>
2019-02-18 21:36:33 -0700

    fixed particles and button

---
+++ 2D_Game/Assets/Scripts/Assignments/Particles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour {
+
+    public ParticleSystem myCoolParticle; 
+
+	// Use this for initialization
+	void Start () {
+         
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ParticleToggle()
+    {
+        if (myCoolParticle.isPlaying)
+        {
+            myCoolParticle.Stop();
+        }
+        else
+        {
+            myCoolParticle.Play();
+        }
+    }
+}
