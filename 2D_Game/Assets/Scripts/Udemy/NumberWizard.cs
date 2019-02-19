50506e1198fe878e4fc2f63e930bb83d4d36572a
Tanner Black <tbb777@gmail.com>
2019-02-18 17:13:58 -0700

    practice with "if" statements

--- 2D_Game/Assets/Scripts/Udemy/NumberWizard.cs
+++ 2D_Game/Assets/Scripts/Udemy/NumberWizard.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Hello World");
-        Debug.Log("Pick a number");
-        Debug.Log("Max number is 1000");
-        Debug.Log("Min number is 1");
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("Welcome to the NumberWizard. Prepare to be amazed!");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("Highest number can be 1000");
+        Debug.Log("Lowest number can be 1");
+        Debug.Log("tell me if your number is higher or lower than " + guess);
+        Debug.Log("Push up arrow if my quess is to low, Push down arrow if my guess is to high, Push enter if my guess is Correct");
+        max = max + 1;
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("is it higher or lower than...");
+            min = guess;
+            guess = (max + min) / 2;
+            Debug.Log(guess);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("is it higher or lower than...");
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log(guess);
+        }
+        else   if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I guessed correctly!");
+        }
+    }
 }
