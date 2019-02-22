e4e43705fee59f716e5b830d0931c55be0dcf601
Tanner Black <tbb777@gmail.com>
2019-02-21 23:00:31 -0700

    Got test to show up at start

--- 2D_Game/Assets/Scripts/Udemy/TextGame.cs
+++ 2D_Game/Assets/Scripts/Udemy/TextGame.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextGame : MonoBehaviour {
 
+    [SerializeField] Text textComponent;
+
 	// Use this for initialization
 	void Start () {
-		
+        textComponent.text = ("Learn to master the magic arts!");
 	}
 	
 	// Update is called once per frame
