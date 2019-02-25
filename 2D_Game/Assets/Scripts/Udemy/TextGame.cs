43b8c8b3ec75d1ca26a06ca00706e148900a59c6
Tanner Black <ybb777@gmail.com>
2019-02-25 16:58:45 -0700

    Merge remote-tracking branch 'origin/master'

--- 2D_Game/Assets/Scripts/Udemy/TextGame.cs
+++ 2D_Game/Assets/Scripts/Udemy/TextGame.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextGame : MonoBehaviour {
 
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
 	// Use this for initialization
 	void Start () {
-		
+        state = startingState;
+        textComponent.text = state.GetStateStory();
 	}
 	
 	// Update is called once per frame
