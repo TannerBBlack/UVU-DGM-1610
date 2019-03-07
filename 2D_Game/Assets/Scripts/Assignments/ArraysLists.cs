2c6e3f5c715c5bc2599b5e81d7185f67e1efdf33
Tanner Black <ybb777@gmail.com>
2019-03-06 18:39:25 -0700

    added array and list script

---
+++ 2D_Game/Assets/Scripts/Assignments/ArraysLists.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysLists : MonoBehaviour {
+
+	int[] oddNumbers = {1, 3, 5, 7, 9};
+
+	// Use this for initialization
+	void Start () {
+		print(oddNumbers[2]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
