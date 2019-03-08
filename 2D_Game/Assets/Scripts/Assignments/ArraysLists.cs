fae82d8498d9ce58f29b949ae5e3b404e3557fa9
Tanner Black <tbb777@gmail.com>
2019-03-07 19:01:07 -0700

    more arrays

--- 2D_Game/Assets/Scripts/Assignments/ArraysLists.cs
+++ 2D_Game/Assets/Scripts/Assignments/ArraysLists.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class ArraysLists : MonoBehaviour {
 
-	int[] oddNumbers = {1, 3, 5, 7, 9};
+	public int[] oddNumbers = {1, 3, 5, 7, 9};
+    public string[] coolJoes = { "Bob", "Joe", "Tanner", "Yugi", "James"};
+   public bool[] didWeWin = {true, false};
+    public string[] dogs = {"Snouser", "Sheperd", "Lab", "Terrier"};
+    public int[] evenNumbers = {2, 4, 6, 8, 10};
+    public string[] videoGames = { "Legend of Zelda", "Startfox", "Halo", "Starcraft", "Valkyria Chronicles" };
+    public string[] goodBooks = { "The Way of Kings", "Steelheart", "Mistborn", "Fablehaven" };
+    public int[] luckyNumbers = { 7, 42, 17, 66, 23 };
+    public float[] funNumbers = { 2.8f, 3.7f, 4.4f };
+    public string[] movies = { "Star Wars", "Shazam", "Lord of the Rings" };
 
 	// Use this for initialization
 	void Start () {
