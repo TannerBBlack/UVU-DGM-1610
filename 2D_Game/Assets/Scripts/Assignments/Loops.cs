7cfcb3adfa7c9f9ef44ccea3b50ec53f96d89bee
Tanner Black <ybb777@gmail.com>
2019-03-25 17:17:02 -0600

    loops and enemy patrol

---
+++ 2D_Game/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    public int bottles = 100;
+    //public bool isRaining = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for loop
+        // for(int i=1; i < bottles; i++){
+        //     print(i+" bottles of tylenol on the wall");
+        // } 
+
+        while(bottles <= 100){
+            print(bottles+" bottles of tylenol on the wall");
+            bottles ++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D_Game/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    // Movement Variables
+    public float moveSpeed;
+    public bool moveRight;
+
+    // Wall Check
+    public Transform wallCheck;
+    public float WallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+
+    // Edge Check
+    private bool notAtEdge;
+    public Transform edgeCheck;
+
+    // Update is called once per frame
+    void Update()
+    {
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, WallCheckRadius, whatIsWall);
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, WallCheckRadius, whatIsWall);
+
+        if (hittingWall || !notAtEdge){
+            moveRight = !moveRight;
+        }
+        
+        if (moveRight){
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        
+    }
+}
