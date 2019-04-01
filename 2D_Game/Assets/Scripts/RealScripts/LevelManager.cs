4d460a0d39a527c84d6885bce75ae0ced12a578f
Tanner Black <ybb777@gmail.com>
2019-04-01 17:16:13 -0600

    LevelManager and KillPlayer

--- 2D_Game/Assets/Scripts/RealScripts/LevelManager.cs
+++ 2D_Game/Assets/Scripts/RealScripts/LevelManager.cs
@@ -23,7 +23,40 @@ public class LevelManager : MonoBehaviour
     //Store Gravity Value
     private float gravityStore;
 
+    // Use this for initialization
     void Start (){
         pcRigid = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+
+    public void RespawnPlayer(){
+        StartCoroutine ("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo(){
+        //Generate death particles
+        Instantiate (deathParticles, pcRigid.transform.position, pcRigid.transform.rotation);
+        //Hide PC
+        player.SetActive(false);
+        player.GetComponent<Renderer> ().enabled = false;
+        //Gravity reset
+        gravityStore = pcRigid.GetComponent<Rigidbody2D>().gravityScale;
+        pcRigid.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        pcRigid.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        //point penalty
+        ScoreManager.AddPoints(-pointPenaltyOnDeath);
+        //debug message
+        Debug.Log ("PC Respawn");
+        //respawn delay
+        yield return new WaitForSeconds (respawnDelay);
+        //gravity restore
+        pcRigid.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+        //match PCs transform position
+        pcRigid.transform.position = currentCheckPoint.transform.position;
+        //show pc
+        player.SetActive(true);
+        pcRigid.GetComponent<Renderer> ().enabled = true;
+        //spawn pc
+        Instantiate (respawnParticles, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
     }
 }
