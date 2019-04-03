d3bb1e7c1d8fd7afb355f4f7e7b858985e200272
Tanner Black <ybb777@gmail.com>
2019-04-03 17:14:45 -0600

    Dictionaries, killswitch

---
+++ 2D_Game/Assets/Scripts/Assignments/Dictionaries.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionaries : MonoBehaviour {
+
+	public Hashtable personalDetails = new Hashtable();
+
+	// Use this for initialization
+	void Start () {
+		personalDetails.Add("firstName", "Greg");
+		personalDetails.Add("lastName", "Lukosek");
+		personalDetails.Add("gender", "male");
+		personalDetails.Add("isMarried", true);
+		personalDetails.Add("age", 29);
+	}
+	
+}
---
+++ 2D_Game/Assets/Scripts/RealScripts/DestroyFinishedParticles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFinishedParticles : MonoBehaviour {
+
+	private ParticleSystem thisParticleSystem;
+
+	// Use this for initialization
+	void Start () {
+		thisParticleSystem = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(thisParticleSystem.isPlaying)
+			return;
+
+		Destroy (gameObject);
+	}
+
+	void OnBecomeInvisible (){
+		Destroy (gameObject);
+	}
+}
