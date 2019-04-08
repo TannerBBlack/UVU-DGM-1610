1f01b2207814e62be104c354c99b3498dfb4e98e
Tanner Black <ybb777@gmail.com>
2019-04-08 17:17:25 -0600

    Project tiles scripts

---
+++ 2D_Game/Assets/Scripts/RealScripts/PlayerShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //:pad Projectile from Resources/Prefabs Folder
+        projectile = Resources.Load("Bullets") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.RightControl)){
+           Instantiate(projectile,firePoint.position, firePoint.rotaion);
+       }
+
+    }
+}
---
+++ 2D_Game/Assets/Scripts/RealScripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+
+    public float timeOut;
+
+    public GameObject enemyDeath;
+
+    public GameObject projectileParticle;
+    
+    public int pointsForKill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     player = GameObject.Find("Player");
+
+     enemyDeath = Resources.Load("Prefabs/Death_PS") as GameObject;
+
+     projectileParticle = Resources.Load("Prefabs/Respawn_PS") as GameObject;
+
+     if(player.transform.localScale.x < 0)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
